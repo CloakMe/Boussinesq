56aec3f47bcb202e5123c890bd77ba7dfbad358b
cloakme <angelow@math.bas.bg>
2022-02-27 13:12:48 +0200

    fix asymptotics

--- BEHyperbolic/1D/twowaves/BPEAsymptotic.m
+++ BEHyperbolic/1D/twowaves/BPEAsymptotic.m
@@ -1,4 +1,5 @@
-function cgreturn = BPEAsymptotic(q,k)
+function cgreturn = BPEAsymptotic(p, q, k, a1, a2, a12)
 
-  cgreturn = -0.2e1 * k ^ 2 / (exp(-0.1e1 * q) + 0.2e1 + exp(q));
+  cgreturn = -0.2e1 * a1 * a12 * k ^ 2 / (a1 ^ 2 * exp(-q) + 0.2e1 * a1 * a12 + a12 ^ 2 * exp(q)) ...
+             -0.2e1 * a2 * a12 * k ^ 2 / (a2 ^ 2 * exp(-p) + 0.2e1 * a12 * a2 + a12 ^ 2 * exp(p));
 end
