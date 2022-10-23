203ae61459ef759d71b709c01efc24e114bb9339
ka <ff@abv.bg>
2022-10-23 14:06:21 +0300

    add plot bnd functionality

--- BEEliptic/Boussinesq2D/PlotBoundary.m
+++ BEEliptic/Boussinesq2D/PlotBoundary.m
@@ -1,19 +1,44 @@
-function PlotBoundary(x,y,zeroX, U, outerTopBoundaryF)
+function PlotBoundary(muU, x, y, U, approxBoundaryF, step, zeroMatrix, derivative)
     
-    augBoundarySize = 0;
-    if(nargin == 4)
-        BND = U(:,end-7:end);
-        augBoundarySize = 4;
-    else
-        BND = [U(:,end-7:end) outerTopBoundaryF];
-    end
+    %[yLen,xLen,yHeiA,xHeiA] = GetInnerNodesForComparingBoundaryFunctions(length(x),length(y),step);
     
-    hy = y(2)-y(1);
-    figure(4);
-    mesh(x(zeroX:end),y(end-11+augBoundarySize:end)+(4-augBoundarySize)*hy,BND');
-    xlabel('x');    ylabel('y');
-    view(-13,16);
-    title('Boundary Smoothness');
+    outerTopBoundaryF=approxBoundaryF(1:end-4,end-3:end); 
+    outerRigthBoundaryF=approxBoundaryF(end-3:end,1:end-4); 
+    dxx_U = XDerivativeEvenFunctions(U, zeroMatrix,muU*outerRigthBoundaryF,derivative.second);
+    dyy_U = YDerivativeEvenFunctions(U, zeroMatrix,muU*outerTopBoundaryF,derivative.second);
+    
+    
+    btU_xx = bt * dxx_U;
+    bt1McSQU_yy = bt * (1-c^2) * dyy_U;
+    
+    coefU_yyyy = - (1 - bt * c^2) * domainUtils.YDerivativeZeroBnd( dyy_U )/h^2;
+    coefU_xxyy = - (2 - bt * c^2) * domainUtils.YDerivativeZeroBnd( dxx_U )/h^2;
+    coefU_xxxx =                  - domainUtils.XDerivativeZeroBnd( dxx_U )/h^2;
+    
+    coeffUsq_xx = + al * bt * domainUtils.XDerivativeZeroBnd(Yk.^2)/h^2;
+    coeffUsq_yy = + al * bt * domainUtils.YDerivativeZeroBnd(Yk.^2)/h^2;
+    
+    %hy = y(2)-y(1);
+    plt = loglog(y(ypts), abs(btU_xx(zeroX, ypts)), 'b',...
+         y(ypts), abs(bt1McSQU_yy(zeroX, ypts)), 'r:',...
+         y(ypts), abs(coeffUsq_xx(zeroX, ypts)), 'm', ...
+         y(ypts), abs(coeffUsq_yy(zeroX, ypts)), 'g',...
+         y(ypts), abs(residual(zeroX, ypts) ), '--', ...
+         y(ypts(1:5:end)), abs( 6 * mu.muU ./ y(ypts(1:5:end)).^4), 'ok', ...
+         y(ypts(1:5:end)), abs( ( coefU_xxyy(zeroX, ypts(1))*y(ypts(1))^6 ) ./ y(ypts(1:5:end)).^6), 'om');% y(zeroY+10:end-barF), 1 ./ y(zeroY+10:end-barF) .^ 2, 'k' ); %(1+end)/2  
+     
+     
+%     figure(4);
+%     mesh(x(xLen), y(yHeiA), abs( U(xLen, yHeiA) - muU*approxBoundaryF(xLen, yHeiA) )' );
+%     xlabel('x');    ylabel('y');
+%     view(0,90);
+%     colorbar;
+%     
+%     figure(5);
+%     mesh(x(xHeiA), y(yLen), abs( U(xHeiA, yLen) - muU*approxBoundaryF(xHeiA, yLen) )' );
+%     xlabel('x');    ylabel('y');
+%     view(0,90);
+%     colorbar;
 end
     %x2 = [x x(end)+h x(end)+2*h x(end)+3*h x(end)+4*h];
     %y2 = [y y(end)+h y(end)+2*h y(end)+3*h y(end)+4*h];
